59af242818ab163058ae07fd2b00712a5607264b
Sanzio Wesley <sanziowesley@hotmail.com>
2020-05-25 20:52:28 -0300

    Tratanto erros com excessao personalizada

--- Xadrez/tabuleiro/Tabuleiro.cs
+++ Xadrez/tabuleiro/Tabuleiro.cs
@@ -17,11 +17,46 @@
         {
             return pecas[linha, coluna];
         }
-       
+
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna]; 
+        }
+
+        //test whether there is a part in a position
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        //throw an exception if the place to move the part is occupied
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Ja existe uma peça nessa posição");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+        //Method for testing whether a position is valid or not
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha<0 || pos.linha>=linhas || pos.coluna<0 || pos.coluna >=colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Launch a custom exception
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida");
+            }
         }
     }
 }
